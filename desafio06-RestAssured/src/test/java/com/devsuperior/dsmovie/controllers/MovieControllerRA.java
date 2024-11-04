e8286f8adee5c44dc2990fde0caf9df66a5cd93c
anapedra <anapedra.mil@gmail.com>
2024-11-04 11:22:13 -0300

    Add: Ensure findAll returns OK with no arguments

--- desafio06-RestAssured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ desafio06-RestAssured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -7,11 +7,22 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class MovieControllerRA {
-	
-	
+
+	private static final String BASE_URI = "http://localhost:8080";
+	private static final String MOVIE_ENDPOINT = "/movies";
+
+
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
-
+		given()
+				.baseUri(BASE_URI)
+				.queryParam("title", "The Witcher")
+				.when()
+				.get(MOVIE_ENDPOINT)
+				.then()
+				.statusCode(200)
+				.body("content.id[0]", is(1))
+				.body("content.title[0]", equalTo("The Witcher"));
 	}
 
 	@Test
