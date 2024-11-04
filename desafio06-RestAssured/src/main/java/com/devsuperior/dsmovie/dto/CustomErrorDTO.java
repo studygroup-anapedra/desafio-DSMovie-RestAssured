eb3ec60fe1933195c5c3fe644a926c77f0897083
anapedra <anapedra.mil@gmail.com>
2024-11-04 12:40:08 -0300

    feat: create CustomErrorDTO for error response handling

---
+++ desafio06-RestAssured/src/main/java/com/devsuperior/dsmovie/dto/CustomErrorDTO.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dsmovie.dto;
+
+import java.time.Instant;
+
+public class CustomErrorDTO {
+
+    private final Instant timestamp;
+    private final Integer status;
+    private final String error;
+    private final String path;
+
+    public CustomErrorDTO(Instant timestamp, Integer status, String error, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.path = path;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
