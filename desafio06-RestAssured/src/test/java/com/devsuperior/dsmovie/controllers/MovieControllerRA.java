9ad536f54208eb0cd3f2da668104217fb216be1e
anapedra <anapedra.mil@gmail.com>
2024-11-04 10:49:12 -0300

    Projeto proposto para o desafio

---
+++ desafio06-RestAssured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -0,0 +1,43 @@
+package com.devsuperior.dsmovie.controllers;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class MovieControllerRA {
+	
+	
+	@Test
+	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+
+	}
+
+	@Test
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+
+	}
+
+	@Test
+	public void findByIdShouldReturnMovieWhenIdExists() {
+
+	}
+
+	@Test
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {
+
+	}
+
+	@Test
+	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {		
+	}
+	
+	@Test
+	public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+	}
+	
+	@Test
+	public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+	}
+}
