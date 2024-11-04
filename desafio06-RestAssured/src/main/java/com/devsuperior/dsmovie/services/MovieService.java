45a2ea9eb5821f4f3a72f84fd9449e7e5096515f
anapedra <anapedra.mil@gmail.com>
2024-11-04 12:07:37 -0300

    feat: implement MovieService for business logic

---
+++ desafio06-RestAssured/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dsmovie.services;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAll(String title, Pageable pageable) {
+		Page<Movie> result = repository.searchByTitle(title, pageable);
+		return result.map(MovieDTO::new);
+	}
+
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Movie result = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+		return new MovieDTO(result);
+	}
+}
