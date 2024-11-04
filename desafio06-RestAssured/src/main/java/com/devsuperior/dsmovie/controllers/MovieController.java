f9621fa8d28b2481c50bb4217f5c251930babfcd
anapedra <anapedra.mil@gmail.com>
2024-11-04 14:55:17 -0300

    Add insert endpoint in MovieController to handle movie creation

--- desafio06-RestAssured/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ desafio06-RestAssured/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -2,10 +2,15 @@ package com.devsuperior.dsmovie.controllers;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.services.MovieService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/movies")
@@ -24,5 +29,13 @@ public class MovieController {
 	@GetMapping(value = "/{id}")
 	public MovieDTO findById(@PathVariable Long id) {
 		return service.findById(id);
+	}
+
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PostMapping
+	public ResponseEntity<MovieDTO> insert(@Valid @RequestBody MovieDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
