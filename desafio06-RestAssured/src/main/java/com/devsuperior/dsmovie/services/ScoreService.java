68f70a1e0551e7ed53a59e7824217512c6acf91e
anapedra <anapedra.mil@gmail.com>
2024-11-05 01:25:37 -0300

    Add ScoreService for business logic related to scores

---
+++ desafio06-RestAssured/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
@@ -0,0 +1,56 @@
+package com.devsuperior.dsmovie.services;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.entities.Score;
+import com.devsuperior.dsmovie.entities.User;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ScoreService {
+
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private MovieRepository movieRepository;
+	
+	@Autowired
+	private ScoreRepository scoreRepository;
+	
+	@Transactional
+	public MovieDTO saveScore(ScoreDTO dto) {
+		
+		User user = userService.authenticated();
+		
+		Movie movie = movieRepository.findById(dto.getMovieId())
+				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+		
+		Score score = new Score();
+		score.setMovie(movie);
+		score.setUser(user);
+		score.setValue(dto.getScore());
+		
+		score = scoreRepository.saveAndFlush(score);
+		
+		double sum = 0.0;
+		for (Score s : movie.getScores()) {
+			sum = sum + s.getValue();
+		}
+			
+		double avg = sum / movie.getScores().size();
+		
+		movie.setScore(avg);
+		movie.setCount(movie.getScores().size());
+		
+		movie = movieRepository.save(movie);
+		
+		return new MovieDTO(movie);
+	}
+}
